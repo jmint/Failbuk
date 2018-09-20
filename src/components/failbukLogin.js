5e5aecf0e6f1a6d9925278433eaac55bb1870027
jmint <joelmiranda1300@gmail.com>
2018-09-20 16:17:53 -0500

    presentationals1

--- src/components/failbukLogin.js
+++ src/components/failbukLogin.js
@@ -63,7 +63,7 @@ const PresentationalIn = () => {
     <p>Hola</p>
 }
 
-const PresentationalOut = () {
+const PresentationalOut = () => {
     <div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
 }
 
